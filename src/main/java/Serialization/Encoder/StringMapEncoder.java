358bcb79ac0db10a0a4274ac255cb1aab7a79777
Dmitry <mitya@mail.ru>
2023-11-30 21:23:35 +0300

    StringMap encoder fixed!

--- src/main/java/Serialization/Encoder/StringMapEncoder.java
+++ src/main/java/Serialization/Encoder/StringMapEncoder.java
@@ -5,25 +5,41 @@ import Serialization.Encoder.Core.IEncoder;
 
 import java.util.*;
 
-public class StringMapEncoder {
+public class StringMapEncoder implements IEncoder<Map<String, String>> {
 
     private static final IEncoder<Integer> intEncoder = new IntEncoder();
     private static final IEncoder<String> stringEncoder = new StringEncoder();
+
+    @Override
     public byte[] encode(Map<String, String> data) {
         List<byte[]> encodedDataList = new LinkedList<>();
         int bytesCounter = 0;
-        for (String key : data.keySet()) {
-            encodedDataList.add(stringEncoder.encode(key));
-            bytesCounter += stringEncoder.encode(key).length;
-            encodedDataList.add(stringEncoder.encode(data.get(key)));
-            bytesCounter += stringEncoder.encode(key).length;
+
+        Set<String> keys = data.keySet();
+        byte[] encodedMapLength = intEncoder.encode(keys.size());
+
+        encodedDataList.add(encodedMapLength);
+        bytesCounter += encodedMapLength.length;
+
+        for (String key : keys) {
+            byte[] encodedKey = stringEncoder.encode(key);
+
+            encodedDataList.add(encodedKey);
+            bytesCounter += encodedKey.length;
+
+            byte[] encodedValue = stringEncoder.encode(data.get(key));
+
+            encodedDataList.add(encodedValue);
+            bytesCounter += encodedValue.length;
         }
 
         byte[] encodedData = new byte[bytesCounter];
         splitList(encodedData, encodedDataList);
+        
         return encodedData;
     }
 
+    @Override
     public DecoderResult<Map<String, String>> decode(byte[] encodedData, int fromByte) {
         DecoderResult<Integer> dataSize = intEncoder.decode(encodedData);
         int offset = fromByte + dataSize.getLength();
@@ -32,15 +48,23 @@ public class StringMapEncoder {
         for (int i = 0; i < dataSize.getDecoderResult(); i++) {
             DecoderResult<String> key = stringEncoder.decode(encodedData, offset);
             offset += key.getLength();
+
             DecoderResult<String> value = stringEncoder.decode(encodedData, offset);
             offset += value.getLength();
+            
             result.put(key.getDecoderResult(), value.getDecoderResult());
         }
         return new DecoderResult<>(result, offset - fromByte);
     }
 
+    @Override
+    public DecoderResult<Map<String, String>> decode(byte[] encodedData) {
+        return decode(encodedData, 0);
+    }
+
     private void splitList(byte[] result, List<byte[]> bytesList) {
         int pos = 0;
+
         for (byte[] bytes : bytesList) {
             System.arraycopy(bytes, 0, result, pos, bytes.length);
             pos += bytes.length;
