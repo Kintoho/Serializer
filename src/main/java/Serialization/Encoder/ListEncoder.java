157f620a7375e0c5bdae5f103f4f57f2e11355de
Dmitry <mitya@mail.ru>
2023-12-02 00:52:50 +0300

    List encoder fixed

--- src/main/java/Serialization/Encoder/ListEncoder.java
+++ src/main/java/Serialization/Encoder/ListEncoder.java
@@ -6,28 +6,50 @@ import Serialization.Encoder.Core.IEncoder;
 import java.util.LinkedList;
 import java.util.List;
 
-public class ListEncoder <V> implements IEncoder<List<V>> {
-    
+public class ListEncoder<V> implements IEncoder<List<V>> {
+    private static final IEncoder<Integer> intEncoder = new IntEncoder();
+    private IEncoder<V> encoder;
+
     @Override
     public byte[] encode(List<V> data) {
         List<byte[]> encodedDataList = new LinkedList<>();
         int bytesCounter = 0;
 
-        IEncoder encoder = switch (data.get(0).getClass().getName()) {
-            case "String" -> new StringEncoder();
-            case "Integer" -> new IntEncoder();
-            case "Long" -> new LongEncoder();
-            case "Double" -> new DoubleEncoder();
-            default -> throw new IllegalStateException("Unexpected value: " + data.get(0).getClass().getName());
-        };
+        if (data.get(0) instanceof String) {
+            encoder = (IEncoder<V>) new StringEncoder();
+        } else if (data.get(0) instanceof Integer) {
+            encoder = (IEncoder<V>) new IntEncoder();
+        } else if (data.get(0) instanceof Long) {
+            encoder = (IEncoder<V>) new LongEncoder();
+        } else if (data.get(0) instanceof Double) {
+            encoder = (IEncoder<V>) new DoubleEncoder();
+        } else {
+            throw new IllegalStateException("Unexpected value: " + data.get(0).getClass());
+        }
+
+        // IEncoder<V> encoder = switch (data.get(0).getClass().getName()) {
+        // case "String" -> new StringEncoder();
+        // case "Integer" -> new IntEncoder();
+        // case "Long" -> new LongEncoder();
+        // case "Double" -> new DoubleEncoder();
+        // default -> throw new IllegalStateException("Unexpected value: " +
+        // data.get(0).getClass().getName());
+        // };
+
+        byte[] encodedListSize = intEncoder.encode(data.size());
+        encodedDataList.add(encodedListSize);
+        bytesCounter += encodedListSize.length;
 
         for (V datum : data) {
-            encodedDataList.add(encoder.encode(datum));
-            bytesCounter += encoder.encode(datum).length;
+            byte[] encodedListItem = encoder.encode(datum);
+
+            encodedDataList.add(encodedListItem);
+            bytesCounter += encodedListItem.length;
         }
 
         byte[] encodedData = new byte[bytesCounter];
-        splitList(encodedData, encodedDataList);
+        listToByteArray(encodedData, encodedDataList);
+
         return encodedData;
     }
 
@@ -38,11 +60,23 @@ public class ListEncoder <V> implements IEncoder<List<V>> {
 
     @Override
     public DecoderResult<List<V>> decode(byte[] encodedData, int fromByte) {
-        return null;
+        DecoderResult<Integer> dataSize = intEncoder.decode(encodedData);
+        int offset = fromByte + dataSize.getLength();
+        List<V> result = new LinkedList<>();
+
+        for (int i = 0; i < dataSize.getDecoderResult(); i++) {
+            DecoderResult<V> listItem = encoder.decode(encodedData, offset);
+            offset += listItem.getLength();
+
+            result.add(listItem.getDecoderResult());
+        }
+        
+        return new DecoderResult<List<V>>(result, offset - fromByte);
     }
 
-    private void splitList(byte[] result, List<byte[]> bytesList) {
+    private void listToByteArray(byte[] result, List<byte[]> bytesList) {
         int pos = 0;
+
         for (byte[] bytes : bytesList) {
             System.arraycopy(bytes, 0, result, pos, bytes.length);
             pos += bytes.length;
