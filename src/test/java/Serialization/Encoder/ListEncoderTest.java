55b01ea679ad5451670b33f9424705ab5acac9e4
Dmitry <mitya@mail.ru>
2023-12-03 23:02:09 +0300

    Nested list tests added

--- src/test/java/Serialization/Encoder/ListEncoderTest.java
+++ src/test/java/Serialization/Encoder/ListEncoderTest.java
@@ -26,7 +26,38 @@ public class ListEncoderTest {
                                 -123456890L)),
                 List.of(
                         new ListEncoder<String>(),
-                        List.of("12ufshgvadbfn;l", "qwertyuioasdfghjkzxcv", "", "123.4567", "/n/n/n/t/t/n")));
+                        List.of("12ufshgvadbfn;l", "qwertyuioasdfghjkzxcv", "", "123.4567", "/n/n/n/t/t/n")),
+                        
+                List.of(
+                        new ListEncoder<List<Long>>(),
+                        List.of(
+                                List.of(Long.MAX_VALUE / 2, Long.MIN_VALUE / 2, -1231231L, 123L, 0L, 20L, -56L, 123456890L,
+                                -123456890L),
+                                List.of(Long.MAX_VALUE / 2, Long.MIN_VALUE / 2, -1231231L, 123L, 0L, 20L, -56L, 123456890L,
+                                -123456890L)
+                        )),
+                List.of(
+                        new ListEncoder<List<String>>(),
+                        List.of(
+                                List.of("12ufshgvadbfn;l", "qwertyuioasdfghjkzxcv", "", "123.4567", "/n/n/n/t/t/n"),
+                                List.of("12ufshgvadbfn;l", "qwertyuioasdfghjkzxcv", "", "123.4567", "/n/n/n/t/t/n")
+                        )),
+                List.of(
+                        new ListEncoder<List<Double>>(),
+                        List.of(
+                                List.of(Double.MAX_VALUE / 2, Double.MIN_VALUE / 2, -1231231.123D, 123.4356D, 0.000001D,
+                                20.1234689D, -56.98765D, 123456890.123456789987D, -123456890.123456789987D),
+                                List.of(Double.MAX_VALUE / 2, Double.MIN_VALUE / 2, -1231231.123D, 123.4356D, 0.000001D,
+                                20.1234689D, -56.98765D, 123456890.123456789987D, -123456890.123456789987D)
+                        )),
+                List.of(
+                        new ListEncoder<List<Integer>>(),
+                        List.of(
+                                List.of(Integer.MAX_VALUE / 2, Integer.MIN_VALUE / 2, -1231231, 123, 0, 20, -56, 123456890,
+                                -123456890),
+                                List.of(Integer.MAX_VALUE / 2, Integer.MIN_VALUE / 2, -1231231, 123, 0, 20, -56, 123456890,
+                                -123456890)
+                        )));
 
         for (var test : testList) {
             var coder = (IEncoder) test.get(0);
